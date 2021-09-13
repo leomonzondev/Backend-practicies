05d04f66b7357a6e916e17e2db9cd94e68569552
leob4rr0s <cristianleonardobm@gmail.com>
2021-09-12 23:00:12 -0300

    app consola

---
+++ 04-aplicacion-consola/app.js
@@ -0,0 +1,16 @@
+require('colors');
+
+const { mostrarMenu } = require('./helpers/mensajes');
+
+
+
+console.clear()
+
+const main = async() => {
+    console.log('Hola mundo');
+
+    mostrarMenu();
+
+}
+
+main();
